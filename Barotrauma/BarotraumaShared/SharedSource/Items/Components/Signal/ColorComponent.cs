ead5685dd70efdbc6ac522c7b5ef504eafa10d7c
Anton Nikolaev <tagrenam@live.ru>
2022-05-11 13:31:26 +0500

    Add dict parsing

--- Barotrauma/BarotraumaShared/SharedSource/Items/Components/Signal/ColorComponent.cs
+++ Barotrauma/BarotraumaShared/SharedSource/Items/Components/Signal/ColorComponent.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Globalization;
+using System.Linq;
 using System.Xml.Linq;
 using Microsoft.Xna.Framework;
 
@@ -9,10 +10,14 @@ namespace Barotrauma.Items.Components
     {
         protected float[] receivedSignal;
 
+        private System.Collections.Generic.Dictionary<string, float> dict = new System.Collections.Generic.Dictionary<string, float>();
+
         private string output = "0,0,0,0";
 
         [InGameEditable, Serialize(false, IsPropertySaveable.Yes, description: "When enabled makes the component translate the signal from HSV into RGB where red is the hue between 0 and 360, green is the saturation between 0 and 1 and blue is the value between 0 and 1.", alwaysUseInstanceValues: true)]
         public bool UseHSV { get; set; }
+        [InGameEditable, Serialize(false, IsPropertySaveable.Yes, description: "When enabled use signal_dict to set values: R:255,G:120,B:123,A:127", alwaysUseInstanceValues: true)]
+        public bool UseDictionary { get; set; }
 
         public ColorComponent(Item item, ContentXElement element)
             : base(item, element)
@@ -29,9 +34,9 @@ namespace Barotrauma.Items.Components
         private void UpdateOutput()
         {
             float signalR = receivedSignal[0],
-                  signalG = receivedSignal[1],
-                  signalB = receivedSignal[2],
-                  signalA = receivedSignal[3];
+                    signalG = receivedSignal[1],
+                    signalB = receivedSignal[2],
+                    signalA = receivedSignal[3];
 
             if (UseHSV)
             {
@@ -39,6 +44,13 @@ namespace Barotrauma.Items.Components
                 signalR = hsvColor.R;
                 signalG = hsvColor.G;
                 signalB = hsvColor.B;
+            }
+            else if (UseDictionary)
+            {
+                dict.TryGetValue("R", out signalR);
+                dict.TryGetValue("G", out signalG);
+                dict.TryGetValue("B", out signalB);
+                dict.TryGetValue("A", out signalA);
             }
 
             output = signalR.ToString("G", CultureInfo.InvariantCulture);
@@ -65,6 +77,22 @@ namespace Barotrauma.Items.Components
                     break;
                 case "signal_a":
                     float.TryParse(signal.value, NumberStyles.Float, CultureInfo.InvariantCulture, out receivedSignal[3]);
+                    UpdateOutput();
+                    break;
+                case "signal_dict":
+                    try
+                    {
+                        char[] separators = new char[] { ' ', ',' };
+                        dict = signal.value.Split(separators, StringSplitOptions.RemoveEmptyEntries)
+                            .Select(x => x.Split(':'))
+                            .ToDictionary(x => x[0], x => float.Parse(x[1]));
+                    }
+                    catch
+                    {
+                        break;
+                    }
+
+
                     UpdateOutput();
                     break;
             }
